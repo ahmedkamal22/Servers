59d2cb581985071fe01413113c946dcbef3aa591
ahmedkamal22 <ak0850000@gmail.com>
2021-05-01 18:17:34 +0300

    In this bransh I inflated custom base classes to give me advantages to use them in several projects also I created fragment classes for every icon Adding several images to the drawable file Creating custom menu for navigation bar

--- app/src/main/java/com/kamal/servers/MainActivity.kt
+++ app/src/main/java/com/kamal/servers/MainActivity.kt
@@ -2,10 +2,44 @@ package com.kamal.servers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.kamal.fragment.fragments.FragmentServer1
+import com.kamal.fragment.fragments.FragmentServer2
+import com.kamal.fragment.fragments.FragmentServer3
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nav1.setOnNavigationItemSelectedListener {
+            if(it.itemId == R.id.navigation_server1)
+            {
+                createFragment(FragmentServer1())
+            }
+            else if(it.itemId == R.id.navigation_server2)
+            {
+                createFragment(FragmentServer2())
+            }
+            else if(it.itemId == R.id.navigation_server3)
+            {
+//                createFragment(FragmentServer3())
+                createDialogeFragment()
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+        nav1.selectedItemId = R.id.navigation_server1
+    }
+    private fun createDialogeFragment() {
+        val dialogue = FragmentServer3()
+        dialogue.show(supportFragmentManager,"")
+    }
+
+    private fun createFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right)
+            .replace(R.id.fragment_container,fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
